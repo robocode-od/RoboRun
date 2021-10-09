9882f163fa0939f8edae1ad6393f30eca84dfca4
robocode-od <robocode13@gmail.com>
2021-10-09 16:18:58 +0300

    Add comment

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    // Это спавнер
+
     [SerializeField] private GameObject _spawnObject;
     [SerializeField] private float _spawnInterval;
 
